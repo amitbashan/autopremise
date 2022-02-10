266e8c039f4a3a15efdeefc4c5585ee1db230d70
amitbashan <54548687+amitbashan@users.noreply.github.com>
2022-02-10 17:36:03 +0200

    Add more group types

--- src/premise/src/api/form_localization/group/input.rs
+++ src/premise/src/api/form_localization/group/input.rs
@@ -40,6 +40,7 @@ pub struct Input {
 #[derive(Debug, Copy, Clone, serde_derive::Serialize, serde_derive::Deserialize)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Style {
+    Decimal,
     SingleLine,
     MultiLine,
     Dropdown,
--- src/premise/src/api/form_localization/group/mod.rs
+++ src/premise/src/api/form_localization/group/mod.rs
@@ -4,8 +4,10 @@ pub mod output;
 #[derive(Debug, Copy, Clone, serde_derive::Serialize, serde_derive::Deserialize)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Type {
+    Number,
     Text,
     Photo,
+    Screenshot,
     #[serde(rename = "GEOPOINT")]
     GeoPoint,
     SelectOne,
